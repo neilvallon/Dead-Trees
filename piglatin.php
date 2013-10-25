bf9d44169cd8a3fa0f0b03b73d4c28c72380c28c
Neil Vallon <neilvallon@gmail.com>
2013-10-24 21:43:36 -0500

    added Hyphen mode suport for qu words and swaped is_vowel function for in_aray
    Jul 25, 2009 - 1:40 AM

--- piglatin.php
+++ piglatin.php
@@ -7,45 +7,58 @@ to see where the script actualy starts, skip down to the comment "//start of scr
 
 */
 
-function is_vowel($firstLetter){ //input: a letter - output: true or false
-$vowels = array("a", "e", "i", "o", "u", "y"); //sets an array to look through for vowels
-if(in_array($firstLetter, $vowels)){ //searches the above array for the first letter of the word
-	$isTrue = true;
-}else{
-	$isTrue = false;
-}
-return $isTrue;	
-}
-
 
 function cons_count($word){ //input: a word - output: number of constanants befor a vowel
-	$a = 0; //counter for the number of constanance
+
+if(substr($word, 0, 2) == "qu"){ //qu word beguining fix so that word like quiet are properly trancelated to ietquay instead of uietqay
+	$a = 2;
+}else{
+	$a = 1; //counter for the number of constanance
 	$i = substr($word, $a, 1); //probably not needed but added just in case to make the while statment false
-	while(!is_vowel($i)){ //while the next letter is not a vowel
+	while(!in_array($i, array("a", "e", "i", "o", "u", "y"))){ //while the next letter is not a vowel
 		$a++; //add one to the contanant count
 		$i = substr($word, $a, 1); //set $i to the lext letter
 	}
+}
 	return $a;
 }
 
 //Start of script
-$msg = strtolower(strip_tags($msg)); //stripts HTML tags and converts the string to lowercase so that the "is_vowel()" function doesnt have to worry about capital letters
+if($hyf == true){
+	$hyf = "-";
+}else{
+	$hyf = "";	
+}
+$msg = strtolower(strip_tags($msg)); //stripts HTML tags and converts the string to lowercase
 
 $words = explode(" ", $msg); //splits the string up into words "hello my name is neil vallon" = array(hello, my, name, is, neil, vallon)
 
 foreach($words as $word){ //for every word in the sentence - convert it to pig latin
 	
 $firstLetter = substr($word, 0, 1); //selects the first letter in the word
-	if(is_vowel($word)){ //if the first letter is a vowel
-	$pigWord = $word."way"; //add "way" to the end and move on to the next word
+	if(in_array($firstLetter, array("a", "e", "i", "o", "u"))){ //if the first letter is a vowel
+	$pigWord = $word.$hyf."way"; //add "way" to the end and move on to the next word
 	}else{ //if it is a constanant
 		$numConst = cons_count($word); //cont the number of constanants before the first vowel ocures
 		$leadingCons = substr($word, 0, $numConst); //starting at the begining of the word selects all the letters at the begining that are constanants and puts them into a veriable
-		$pigWord = substr($word, $numConst).$leadingCons."ay"; //selects the word without the leading constanants and put them on the end with "ay" then moves on to the word
+		$pigWord = substr($word, $numConst).$hyf.$leadingCons."ay"; //selects the word without the leading constanants and put them on the end with "ay" then moves on to the word
 	}
 	
-	$endStr .= $pigWord." "; //puts the pig latin word that was just generated on to the end of string so far with a space
+	/*
 	
+	
+	future work here
+	
+	make a loop that hapens for each word that get the length of the string and goes all the way through searching for punctuation then moveing them to the end
+	
+	this may be better suited for the begining of the loop before the vowel check to esure that a 500 error does not ocure
+	
+	
+	*/
+	
+	
+	
+	$endStr .= $pigWord." "; //puts the pig latin word that was just generated on to the end of string so far with a space
 }
 
 echo $endStr; //echos the finished sentence
