4ed8e8cf33496092f5b2679ee36c4fa79f683e7d
Neil Vallon <neilvallon@gmail.com>
2015-04-02 11:59:38 +0000

    first known snapshot of permutation algorithm
    found in "permutation.zip"
    
    index:       Feb 28, 2010, 12:54 PM
    result:      Feb 28, 2010,  2:22 AM
    swapPattern: Feb 28, 2010, 12:54 PM

---
+++ result.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>The Permutation are Endless</title>
+</head>
+<body>
+<?php
+function fix_for_page($value){
+	$value = htmlspecialchars(trim($value));
+	if(get_magic_quotes_gpc())
+		$value = stripslashes($value);
+	return $value;
+}
+$deck = explode(",", fix_for_page($_REQUEST['dataSet']));
+if(sizeof($deck) >10) die("To Many Values");
+$time_start = microtime(true);
+require("swapPattern.php");
+?>
+<br /><br /><br /><div style="position:absolute;top:10px;right:10px;font-size:12px;color:#616161;text-align:center;">
+<?php
+$time_end = microtime(true);
+$time = $time_end - $time_start;
+echo 'Generated: '.factorial(sizeof($deck))." results - in: ".round($time, 4)." seconds.";
+?>
+<br />&copy; 2009 - Neil Vallon</div>
+</body>
+</html>
---
+++ swapPattern.php
@@ -0,0 +1,52 @@
+<?php
+function factorial($num) { // 5!
+	if ($num == 0) return 1;
+	return $num * factorial($num - 1);
+}
+
+function place2end($deck, $pos){ //Rotate
+	global $deckSize;
+	$deck[($deckSize+1)] = $deck[$pos];
+	array_splice($deck, $pos, 1);
+	return $deck;
+}
+
+function getNextDeck($deck, $iteration){
+	global $deckSize;
+	
+	//The magical bit
+	for($i=0;$i<$deckSize;$i++){
+		$rotationThreshold = factorial($deckSize-$i);
+		$numRotations = floor($iteration/$rotationThreshold);
+		for($x=0;$x<$numRotations;$x++) $deck = place2end($deck, $i-1);
+		$iteration = $iteration - ($numRotations*$rotationThreshold);
+	}
+	//That was some David Blane shit right there, right?
+	
+	return $deck;
+}
+//Below this line I don't consider a vital part of the script
+
+function printDeck($deck){
+	global $deckSize;
+	for($i=0;$i<$deckSize;$i++) echo " ".$deck[$i];
+}
+
+function printAll($deck){
+	global $deckSize;
+	for($i=0;$i<factorial($deckSize);$i++){
+		printDeck(getNextDeck($deck, $i));
+		echo "<br />";
+	}
+}
+
+//$deck = array("2", "3", "4", "5", "3", "4", "5", "3", "4", "5", "3", "4", "5");
+
+$deckSize = sizeof($deck);
+
+printAll($deck);
+
+/*
+$deck = array("A-C", "A-D", "A-H", "A-S", "K-C", "K-D", "K-H", "K-S", "Q-C", "Q-D", "Q-H", "Q-S", "J-C", "J-D", "J-H", "J-S", "10-C", "10-D", "10-H", "10-S", "9-C", "9-D", "9-H", "9-S", "8-C", "8-D", "8-H", "8-S", "7-C", "7-D", "7-H", "7-S", "6-C", "6-D", "6-H", "6-S", "5-C", "5-D", "5-H", "5-S", "4-C", "4-D", "4-H", "4-S", "3-C", "3-D", "3-H", "3-S", "2-C", "2-D", "2-H", "2-S");
+*/
+?>
